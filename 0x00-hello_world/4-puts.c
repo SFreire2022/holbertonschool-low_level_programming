257cc5e172b19ea74cc6a0d843bf421b9b799807
SFreire2022 <4437@holbertonschool.com>
2022-02-03 09:40:08 -0800

    script 4 added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main program
+ *
+ * Description: First use of puts for Holberton
+ * Return: If all goes on, it returns 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
