9aef32b83c0a8544092624fe05e7266435d74612
SFreire2022 <4437@holbertonschool.com>
2022-02-03 10:04:29 -0800

    C source code for exercise 5 added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main program
+ *
+ * Description: First use of printf for Holberton
+ * Return: If all goes on, it returns 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
