c4fef46fb83e0900844e1458faf370df2d5d576f
SFreire2022 <4437@holbertonschool.com>
2022-02-07 07:23:22 -0800

    first exercice for functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main program
+ *
+ * Description: Display "_putchar" not using standar libraries
+ * Return: If all goes on, it returns 0
+ */
+int main(void)
+{
+	char string[] = "_putchar";
+	int n, size;
+
+	size = sizeof(string);
+	for (n = 0; n < size; n++)
+		_putchar(string[n]);
+
+	_putchar('\n');
+
+return (0);
+}
