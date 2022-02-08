51c9dbb3de4eef9946226ed0dadc4707332ee728
SFreire2022 <4437@holbertonschool.com>
2022-02-07 16:33:46 -0800

    Function for exercice 10-add.c and main.h update for function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Return a + b result.
+ *
+ * @a: argument number as an integer.
+ * @b: argument number as an integer.
+ * Return: a + b result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
