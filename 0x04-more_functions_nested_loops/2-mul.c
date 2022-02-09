8696cddae0e43c0df2fdcb3ca2e0ce238974bf6f
SFreire2022 <4437@holbertonschool.com>
2022-02-09 05:36:07 -0800

    Source code function for exercice 2-mul.c and main.h update prototype function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: argument number as an integer.
+ * @b: argument number as an integer.
+ * Return: a * b result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
