c883c61831e53217681ff9eb397caa04180dc30a
SFreire2022 <4437@holbertonschool.com>
2022-02-09 08:54:13 -0800

    fix code function for exercice 5-more_numbers.c - reduce code  and main.h update prototype function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,42 +1,24 @@
 #include "main.h"
 
 /**
- * putchars - print recursively all cahrs from an int number.
- * @n: spects intger as an argument
- *
- * Return: Void
- */
-void putchars(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-	{
-		putchars(n / 10);
-	}
-
-	_putchar(n % 10 + '0');
-}
-
-/**
  * more_numbers - Print 10 times numbers til 14.
  *
  * Return: Always 0
  */
 void more_numbers(void)
 {
-int n;
-int a;
+int n, a;
 
 for (a = 0; a < 10; a++)
 {
 	for (n = 0; n <= 14; n++)
-		putchars(n);
-
+	{
+		if (n > 9)
+		{
+			_putchar(n / 10 + '0');
+		}
+		_putchar(n % 10 + '0');
+	}
 	_putchar('\n');
 }
 }
