b1194ad625a33365bb7a7da5d5caa33cdd1baa82
SFreire2022 <4437@holbertonschool.com>
2022-02-11 09:32:56 -0800

    push README.md, main.h and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Cange the value o an external variable using
+ * Dereferencing.
+ *
+ * @n: The pointer to addres of given var as a param.
+ *
+ * Return: Void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
