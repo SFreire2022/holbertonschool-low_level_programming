360e6d2469a972b266530a0ab35ff1636ac25a29
SFreire2022 <4437@holbertonschool.com>
2022-02-11 17:27:50 -0800

    C source code upload for exercie 4-rev_string and prototype function in main.h updated

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse each char of a pointer to
+ * addres of given string using dereferencing.
+ *
+ * @s: The pointer to addres of given string as a param.
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
