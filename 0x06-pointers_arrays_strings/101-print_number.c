80d7a0999eecca67c4773185536fc15069da8991
SFreire2022 <4437@holbertonschool.com>
2022-02-15 17:54:50 -0800

    Upload function 101-print_number and funtion prototype in main.h

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - Print a given integer using _putchar.
+ * @n: input integer.
+ * Return: Void.
+ */
+void print_number(int n)
+{
+unsigned int d, count, a;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = n * -1;
+	}
+	else
+		a = n;
+
+	d = a;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+		_putchar(((a / count) % 10) + '0');
+
+}
