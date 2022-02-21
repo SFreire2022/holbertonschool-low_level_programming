f56eb27ffbe72cc30fd71493a7807b7b974e17a3
SFreire2022 <4437@holbertonschool.com>
2022-02-21 04:48:25 -0800

    Upload function 0-memset, funtion prototype in main.h and README.md

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Fill memory with a constant byte
+ * @s: buffer array
+ * @b: constant byte
+ * @n: number of bytes of memory area to fill
+ * Description: Fill the first `n` bytes of the memory area pointed to by `s`
+ * with the constant byte `b`.
+ * Return: Pointer to memory area `s`
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+		s[i] = b;
+
+	return (s);
+}
