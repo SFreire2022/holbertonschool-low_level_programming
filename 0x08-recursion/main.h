8693384c7725869f1d60d64870d08ac45a6d2d51
SFreire2022 <4437@holbertonschool.com>
2022-02-23 09:40:28 -0800

    upload 5-sqrt_recursion and update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int counter(int num, int i);
 int is_prime_number(int n);
 
 #endif
