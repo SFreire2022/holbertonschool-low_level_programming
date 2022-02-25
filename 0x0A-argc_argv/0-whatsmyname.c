01f0163ad855e71fd9d749cf7da3fa1c7fbd9399
SFreire2022 <4437@holbertonschool.com>
2022-02-25 14:56:28 -0800

    Fix argument not used for exercises 0 and 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - Prints the name of this program, followed by a new line.
@@ -6,7 +5,7 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
