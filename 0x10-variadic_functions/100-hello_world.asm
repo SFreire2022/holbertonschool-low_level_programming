9a3c4cdabd884b3165adcde5aaeefa2be9c869f8
SFreire2022 <4437@holbertonschool.com>
2022-03-10 18:59:06 -0800

    fix syscall Hello, World asembler

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,16 +1,14 @@
 section .data
-	msg db "Hello, World",10
+	msg db	"Hello, World", 0xa
 
-	section .text
+section .text
 	global main
-
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, msg
+	mov	rdx, 17
 	syscall
-
-	mov rax, 60
-	mov rdi, 0
+	mov    rax, 60
+	mov	rdi, 0
 	syscall
