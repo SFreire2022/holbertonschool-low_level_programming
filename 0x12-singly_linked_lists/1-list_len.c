f287ad948a2ea90c643313caf4e6b400561831f3
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-17 19:05:48 -0300

    Correct description and add function to traverse and count elements for a given list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,19 @@
 #include "lists.h"
-
 /**
- * print_list - Prints all the elements of a list_t list.
- * @n: Pointer to the first element of the list.
+ * list_len - Traverse all elements in a list to count them.
+ * @h: Pointer to the first element of the list.
  * Return: Number of elements in the list.
  */
-size_t print_list(const list_t *n)
+size_t list_len(const list_t *n)
 {
 	size_t elements = 0;
 	/* size_t in 64 bit arch would be a typedef for unsigned long long */
 	/* size_t to handle big number of elements linked list*/
 	/* const list_t *n To keep constant the pointer to first element */
 
+	elements = 0;
 	while (n != NULL)
 	{
-		if (n->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", n->len, n->str);
 		n = n->next;
 		elements++;
 	}
