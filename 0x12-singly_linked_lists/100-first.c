351f172192758c3edec5fa114c281b6f6248efeb
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-18 09:40:40 -0300

    update description

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,7 @@ void no_main_yet(void) __attribute__ ((constructor));
 /**
  * no_main_yet - Executes functions before main
  * Constructor Attribute allows the function to run before main
+ * Passing function no_main_yet as an atribute to the constructor.
  * Whereas the Deconstructor attribute pushes the executions after main.
  */
 void no_main_yet(void)
