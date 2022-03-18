0bbfc0f6687091d54c1894466fb2a945eead249d
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-18 09:40:14 -0300

    More memotec name for the function executed befor main and expand description

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void before_main(void) __attribute__ ((constructor));
+void no_main_yet(void) __attribute__ ((constructor));
 /**
- * before_main - Executes functions before main
+ * no_main_yet - Executes functions before main
  * Constructor Attribute allows the function to run before main
  * Whereas the Deconstructor attribute pushes the executions after main.
  */
-void before_main(void)
+void no_main_yet(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
