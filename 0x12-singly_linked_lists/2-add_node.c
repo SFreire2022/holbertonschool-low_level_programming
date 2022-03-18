bca7f1f207ec6ee3d0acb04a9e45ab8753ba0267
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-17 22:50:45 -0300

    Create function to add nwe node to the list with type list_t

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to the list
+ * of a list_t type.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
