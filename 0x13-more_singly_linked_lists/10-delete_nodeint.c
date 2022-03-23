69bfc26febbc00c3016da6434ad6a8e0ca7104ed
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-22 23:12:07 -0300

    fix valgrind error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,51 +1,39 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes node at the given position.
- * @head: contains the address of head
- * @index: what node to delete
- * Return: -1 | 1
+ * delete_nodeint_at_index - delete the node at index of a `listint_t` list
+ * @head: double header to head of linked list
+ * @index: index of node to delete, starting at 0
+ * Return: 1 if success, -1 if failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head; /* copies *head contents to temp */
-	listint_t *afternode; /* Ptr to keep the addr of nxt elem from deltd one*/
-	unsigned int nnode; /* node counter */
+	listint_t *current;
+	listint_t *hold;
+	unsigned int i;
 
-	/* General checker if *head/head == NULL */
-	if (*head == NULL || head == NULL)
-	{
-		free(*head);
+	i = 0;
+	current = *head;
+	if (*head == NULL)
 		return (-1);
+
+	while (i < index)
+	{
+		i++;
+		hold = current;
+		if (current->next)
+			current = current->next;
+		else
+			return (-1);
 	}
 
 	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-	}
-
+		*head = current->next;
+	else if (current->next)
+		hold->next = current->next;
 	else
-	{
-		for (nnode = 0; nnode < index; nnode++)
-		{
-			if (temp == NULL)
-			{
-				return (-1);
-			}
-			temp = temp->next; /* Get to the required index + 1 (TBDel)*/
-		}
+		hold->next = NULL;
 
-		afternode = temp; /* copy the address of next */
-		free(temp); /* remove the current node */
-
-		/* linked lists can't be indexed in reverse, get to the required index */
-		temp = *head; /* resetting temp */
-		for (nnode = 0; nnode < index - 1; nnode++)
-		{
-			temp = temp->next; /* get to the required index */
-		}
-		temp->next = afternode->next; /* copy addr of node after deleted node */
-	}
-return (1);
+	free(current);
+	return (1);
 }
