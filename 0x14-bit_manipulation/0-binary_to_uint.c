f2c62a64eb7ecfd37bdbe8883c3b52bcc391ac5f
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-25 01:55:24 -0300

    Delete macro NULL and add more descriptions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,19 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int tot, pow;
 	int len;
 
-	if (b == NULL)
+	if (b == '\0') /* Check if empty */
 		return (0);
 
-	for (len = 0; b[len]; len++)
+	for (len = 0; b[len]; len++) /* Count bin length */
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[len] != '0' && b[len] != '1') /* check if not binary as input*/
 			return (0);
 	}
-
+	/* Start from LSB +1 position decreasing len to MSB */
 	for (pow = 1, tot = 0, len--; len >= 0; len--, pow *= 2)
 	{
-		if (b[len] == '1')
-			tot += pow;
+		if (b[len] == '1') /* Sum position value only when have 1 */
+			tot += pow; /* pow = 2 ^ position starting from pos 1 from R to L */
 	}
 
 	return (tot);
