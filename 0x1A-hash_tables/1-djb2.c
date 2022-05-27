ad8d3fa19058885d9baccaa700ed081b5381f655
SFreire2022 <sebastiancazenave@gmail.com>
2022-05-27 15:52:30 -0300

    update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,7 +3,6 @@
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
- *
  * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
@@ -16,5 +15,5 @@ unsigned long int hash_djb2(const unsigned char *str)
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-	return (hash); 
+	return (hash);
 }
