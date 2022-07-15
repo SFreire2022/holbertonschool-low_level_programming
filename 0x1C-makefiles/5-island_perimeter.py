5f789f12317fbebe8d30ccfdd56089061c8e3578
SFreire2022 <sebastiancazenave@gmail.com>
2022-07-14 22:19:22 -0300

    Adition for different condition cases

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,10 +7,16 @@ def island_perimeter(grid):
     perimeter = 0
 
     if grid:
-        for a in range(len(grid)):
-            for b in range(len(grid[0])):
-                if grid[a][b] == 1:
-                    perimeter = perimeter + 1
-        perimeter = (perimeter * 2) + 2
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if grid[y][x] == 1:
+                    if (y - 1) == -1 or grid[y - 1][x] == 0:
+                        perimeter += 1
+                    if (y + 1) == len(grid) or grid[y + 1][x] == 0:
+                        perimeter += 1
+                    if (x - 1) == -1 or grid[y][x - 1] == 0:
+                        perimeter += 1
+                    if (x + 1) == len(grid[0]) or grid[y][x + 1] == 0:
+                        perimeter += 1
 
     return perimeter
