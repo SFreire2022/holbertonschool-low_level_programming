120f212578f29c4d196ac7dea81079c748785f6e
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-29 11:34:47 -0300

    Add project library

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef S_ALG_H
+#define S_ALG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
